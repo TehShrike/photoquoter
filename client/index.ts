697a719d40218bb5823d3b169baaf56f1506f11d
Josh Duff <me@JoshDuff.com>
2023-11-11 10:51:15 -0600

    need dat livereload

--- client/index.ts
+++ client/index.ts
@@ -1,8 +1,18 @@
 import log_thingy from './import_me.ts'
 import Test from './Test.svelte'
 
-log_thingy('sup')
+log_thingy('sup did you defer')
 
 new Test({
 	target: document.getElementById('target'),
 })
+
+console.log('writing and stuff')
+
+const livereload_port = 35730
+
+const script = document.createElement(`script`)
+script.src = `http://${
+	(location.host || `localhost`).split(`:`)[0]
+}:${livereload_port}/livereload.js?snipver=1`
+document.body.appendChild(script)
